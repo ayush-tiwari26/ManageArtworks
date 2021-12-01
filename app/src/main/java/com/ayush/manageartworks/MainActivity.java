e034aa8e1cb1e64d5643c8e3ba3d2c39a47f0eff
Ayush Tiwari <ayushtiwari26402@gmail.com>
2021-12-01 18:27:29 +0530

    Enabled load pdf from local storage feature.

--- app/src/main/java/com/ayush/manageartworks/MainActivity.java
+++ app/src/main/java/com/ayush/manageartworks/MainActivity.java
@@ -1,23 +1,50 @@
 package com.ayush.manageartworks;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import java.io.File;
 import com.pdftron.pdf.config.ViewerConfig;
 import com.pdftron.pdf.controls.DocumentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LOAD_REQUEST_CODE = 1;
+    private static final String TAG = "Main Activity Log";
+    ViewerConfig config;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        config = new ViewerConfig.Builder().openUrlCachePath(this.getCacheDir().getAbsolutePath()).build();
+    }
 
+    public void loadPdfFromStorage(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/pdf");
+        startActivityForResult(intent, LOAD_REQUEST_CODE);
+    }
 
-        ViewerConfig config = new ViewerConfig.Builder().openUrlCachePath(this.getCacheDir().getAbsolutePath()).build();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==LOAD_REQUEST_CODE){
+            Uri uri = data.getData();
+            Log.d(TAG,"Pdf selected"+uri);
+            DocumentActivity.openDocument(this, uri, config);
+        }
+    }
+
+    public void loadDemoPdf(View view) {
         final Uri fileLink = Uri.parse("https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf");
         DocumentActivity.openDocument(this, fileLink, config);
     }
